c50032040bcff519afbb01ad173c4b5a4590ceda
Adrien Cosson <ad.cosson@gmail.com>
2020-06-11 15:41:07 -0400

    Fixed loop for the TV

--- hardware/tv.py
+++ hardware/tv.py
@@ -2,18 +2,21 @@ import vlc
 import pafy
 from time import sleep
 
+rickroll: str = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+short: str = "https://www.youtube.com/watch?v=C0DPdy98e4c"
+
 
 class TV():
     "Simple wrapper for a PIR device "
 
-    def __init__(self, media: str = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"):
+    def __init__(self, media: str = "https://www.youtube.com/watch?v=C0DPdy98e4c"):
         # Get the correct media URL
         video = pafy.new(media)
         best = video.getbest()
         playurl: str = best.url
         self.url = playurl
 
-        self.instance = vlc.Instance("--vout=dummy", "--aout=dummy", "--input-repeat=-1")
+        self.instance = vlc.Instance("--vout=dummy", "--aout=dummy", "--input-repeat=99999")
         self.player: vlc.MediaPlayer = self.instance.media_player_new()
         self.media: vlc.Media = self.instance.media_new(self.url)
         self.media.get_mrl()
@@ -47,6 +50,6 @@ if __name__ == "__main__":
 
     tv = TV()
     tv.on()
-    while tv.state is True:
+    while True:
         print(tv.state)
         sleep(1)
